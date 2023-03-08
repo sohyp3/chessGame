7c3dbfdf8c531d440e693b28e0ed808c7ee9e3f6
sohyp3 <0xshb@protonmail.com>
2023-03-09 00:47:11 +0300

    looks

--- game/urls.py
+++ game/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from . import views
 urlpatterns = [
+    path('',views.chooseMode,name='chooseMode'),
     path('play',views.playLocal,name='playLocal'),
     path('ai',views.playAI,name='playAI'),
-    path('',views.chooseMode,name='chooseMode'),
     path('board',views.board),
     path('reset',views.resetBoard,name='resetBoard')
 ]
